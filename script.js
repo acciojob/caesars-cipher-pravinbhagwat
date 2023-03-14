7b7c9c48a8c7080e0714305179b5a6dbcdbf1731
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-03-14 19:50:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Your Script here.
+function rot13(str) {
+  var decoded = "";
+  for (var i = 0; i < str.length; i++) {
+    var c = str.charCodeAt(i);
+    if (c >= 65 && c <= 90) { // check if character is uppercase letter
+      decoded += String.fromCharCode((c - 65 + 13) % 26 + 65); // decode using ROT13 algorithm
+    } else {
+      decoded += str.charAt(i); // non-alphabetic character, pass it on
+    }
+  }
+  return decoded;
+}
